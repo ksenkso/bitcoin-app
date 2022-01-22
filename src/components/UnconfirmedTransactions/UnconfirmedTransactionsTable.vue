497b8800be073bd4533c0bf7e23d975570ff51d3
ksenkso <yazunya@gmail.com>
2022-01-22 21:59:13 +0300

    feat: add a table for transactions

---
+++ src/components/UnconfirmedTransactions/UnconfirmedTransactionsTable.vue
@@ -0,0 +1,115 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th class="address">
+          From
+        </th>
+        <th class="address">
+          To
+        </th>
+        <th class="sum">
+          Sum
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="message in messages"
+        :key="message.hash"
+      >
+        <td>
+          <Link
+            v-for="input in message.from"
+            :key="input"
+            :href="`https://www.blockchain.com/btc/address/${input}`"
+            target="_blank"
+          >
+            {{ input }}
+          </Link>
+        </td>
+        <td>
+          <Link
+            v-for="out in message.to"
+            :key="out"
+            :href="`https://www.blockchain.com/btc/address/${out}`"
+            target="_blank"
+          >
+            {{ out }}
+          </Link>
+        </td>
+        <td>{{ btcToString(message.value) }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { btcToString } from '@/utils'
+import { UnconfirmedBTCTransaction } from '@/composables/useBitcoin'
+import Link from '@/components/Link.vue'
+
+export default defineComponent({
+  name: 'UnconfirmedTransactionsTable',
+  components: { Link },
+  props: {
+    messages: {
+      type: Array as PropType<UnconfirmedBTCTransaction[]>,
+      required: true,
+    },
+  },
+  setup () {
+    return {
+      btcToString,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import "~@/styles/_mixins.scss";
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid grey;
+  table-layout: fixed;
+
+  thead {
+    top: 0;
+    background-color: #fff;
+  }
+
+  th, td {
+    border: 1px solid grey;
+  }
+
+  th {
+    font-weight: bold;
+    padding: 8px;
+
+    &.address {
+      width: 40ch;
+
+      @include on-md {
+        width: auto;
+      }
+    }
+
+    &.sum {
+      width: 16ch;
+    }
+  }
+
+  td {
+    padding: 4px;
+
+    .link {
+      display: block;
+      white-space: normal;
+      word-break: break-all;
+    }
+  }
+}
+</style>
