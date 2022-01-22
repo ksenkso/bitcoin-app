1994a05202e4ea23068a6e4c4059bfcfa571047a
ksenkso <yazunya@gmail.com>
2022-01-23 00:57:02 +0300

    feat: add `reconnect` feature

--- src/composables/useWebSocket.ts
+++ src/composables/useWebSocket.ts
@@ -1,4 +1,5 @@
 import { Ref, ref } from 'vue'
+import { log } from '@/utils'
 
 export type WebSocketOptions = ConstructorParameters<typeof WebSocket>
 
@@ -6,23 +7,92 @@ export type WebSocketHook = {
   ready: Ref<boolean>;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   send: (data: any) => void;
+  close: () => void;
+  reconnect: () => void;
   onOpen: (fn: (event: Event) => void) => void;
   onClose: (fn: (event: CloseEvent) => void) => void;
   onError: (fn: (event: Event) => void) => void;
   onMessage: (fn: (event: MessageEvent) => void) => void;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnyFunction = (...args: any[]) => any
+
 export function useWebSocket (...options: WebSocketOptions): WebSocketHook {
   const ready = ref<boolean>(false)
+  const listeners = new Map<keyof WebSocketEventMap, AnyFunction[]>([
+    ['open', []],
+    ['close', []],
+    ['message', []],
+    ['error', []],
+  ])
 
-  const socket = new WebSocket(...options)
+  const addEventListener = <T extends keyof WebSocketEventMap = keyof WebSocketEventMap> (event: T, fn: (event: WebSocketEventMap[T]) => void) => {
+    socket.addEventListener(event, fn)
+    const eventListeners = listeners.get(event)
+    if (eventListeners) {
+      eventListeners.push(fn)
+    }
+  }
 
-  socket.addEventListener('open', () => {
+  const removeEventListener = <T extends keyof WebSocketEventMap = keyof WebSocketEventMap> (event: T, fn: (event: WebSocketEventMap[T]) => void) => {
+    socket.removeEventListener(event, fn)
+    const eventListeners = listeners.get(event)
+    if (eventListeners) {
+      const index = eventListeners.indexOf(fn)
+      if (index !== -1) {
+        eventListeners.splice(index, 1)
+      }
+    }
+  }
+
+  let socket = new WebSocket(...options)
+
+  const reconnect = (): Promise<void> => {
+    socket = new WebSocket(...options)
+    for (const [event, handlers] of listeners.entries()) {
+      handlers.forEach((handler) => socket.addEventListener(event, handler))
+    }
+
+    return new Promise((resolve, reject) => {
+      const ok = () => {
+        removeEventListener('open', ok)
+        resolve()
+      }
+      const error = (event: Event) => {
+        removeEventListener('error', error)
+
+        reject(event)
+      }
+
+      addEventListener('open', ok)
+      addEventListener('error', error)
+    })
+  }
+
+  const close = () => {
+    listeners.clear()
+    listeners.set('open', [])
+    listeners.set('close', [])
+    listeners.set('message', [])
+    listeners.set('error', [])
+    socket.close()
+  }
+
+  addEventListener('open', () => {
     ready.value = true
   })
 
-  socket.addEventListener('close', () => {
+  addEventListener('close', () => {
     ready.value = false
+  })
+
+  addEventListener('error', (event) => {
+    if (socket.readyState !== socket.OPEN) {
+      ready.value = false
+    }
+
+    log('error', 'socket', { socket, event, options })
   })
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -31,24 +101,26 @@ export function useWebSocket (...options: WebSocketOptions): WebSocketHook {
   }
 
   const onOpen = (fn: (event: Event) => void) => {
-    socket.addEventListener('open', fn)
+    addEventListener('open', fn)
   }
 
   const onClose = (fn: (event: CloseEvent) => void) => {
-    socket.addEventListener('close', fn)
+    addEventListener('close', fn)
   }
 
   const onError = (fn: (event: Event) => void) => {
-    socket.addEventListener('error', fn)
+    addEventListener('error', fn)
   }
 
   const onMessage = (fn: (event: MessageEvent) => void) => {
-    socket.addEventListener('message', fn)
+    addEventListener('message', fn)
   }
 
   return {
     ready,
     send,
+    close,
+    reconnect,
     onOpen,
     onClose,
     onError,
