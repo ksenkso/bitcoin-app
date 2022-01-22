afec36eae10038e982dc414bfa3928192ccec04e
ksenkso <yazunya@gmail.com>
2022-01-23 01:40:09 +0300

    feat: add Loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,30 @@
+<template>
+  <img
+    class="loader"
+    src="@/assets/loader.svg"
+    alt="Загрузка"
+  >
+</template>
+
+<script>
+export default {
+  name: 'Loader',
+}
+</script>
+
+<style lang="scss" scoped>
+.loader {
+
+  animation: 2s linear infinite spin;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
