14624a062536207fabdd23d102f59fbd1e3469a0
ksenkso <yazunya@gmail.com>
2022-01-23 04:09:54 +0300

    tests: add tests on stopping listening

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,32 @@
-// https://docs.cypress.io/api/introduction/api.html
+describe('Тест управления сокетами', () => {
+  const WAIT_BEFORE_STOP = 2000
+  const WAIT_AFTER_STOP = 4000
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+  beforeEach(() => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+  })
+
+  it('новые строки не должны добавляться после того, как была нажата кнопка "Остановка"', () => {
+    cy.contains('Запуск').click()
+    cy.wait(WAIT_BEFORE_STOP)
+
+    cy.get('table').find('tr').then(rows => {
+      const count = Cypress.$(rows).length
+      cy.contains('Остановка').click()
+      cy.wait(WAIT_AFTER_STOP)
+      cy.get('tr').should('have.length', count)
+    })
+  })
+
+  it('сумма не должна изменяться после того, как была нажата кнопка "Остановка"', () => {
+    cy.contains('Запуск').click()
+    cy.wait(WAIT_BEFORE_STOP)
+
+    cy.get('table').find('tr').then(() => {
+      const sum = Cypress.$('.sum').text()
+      cy.contains('Остановка').click()
+      cy.wait(WAIT_AFTER_STOP)
+      cy.get('.sum').should('have.text', sum)
+    })
   })
 })
